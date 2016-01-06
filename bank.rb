84d0084207e7bb69a76463e2fa2fe60812e4a099
dianacamacho <dcam1901@yahoo.com>
2016-01-05 21:07:40 -0600

    bank edits headers

--- bank.rb
+++ bank.rb
@@ -1,6 +1,6 @@
 accounts = []
 
-5.times do
+2.times do
 
   account_hash ={}
 
@@ -12,9 +12,10 @@ accounts = []
 
   puts "Please enter an email address"
   email_address = gets.chomp
+
     while true
       if
-        email_address.include?("@") && email_address.end_with?(".com")
+        email_address.include?("@") && email_address.end_with?(".com", ".edu", ".net")
         account_hash[:email] = email_address
         break
       else 
@@ -24,6 +25,8 @@ accounts = []
       end
     end
 
+    puts "--------------------------------"
+
   random_account_number = rand(9999999999).to_s.center(10, rand(0..9).to_s)
   account_hash[:account_number] = random_account_number
 
@@ -31,22 +34,30 @@ accounts = []
 
 end
 
-accounts.each do |hash|
-  puts "FIRST NAME: #{hash[:first_name]}"
-  puts "LAST NAME: #{hash[:last_name]}"
-  puts "EMAIL: #{hash[:email]}"
-  puts "ACCT #: #{hash[:account_number]}"
-  puts "--------------"
+puts "ACCOUNTS INFORMATION:"
+puts "--------------------------------"
+
+accounts.each do |account|
+  puts "FIRST NAME: #{account[:first_name]}"
+  puts "LAST NAME: #{account[:last_name]}"
+  puts "EMAIL: #{account[:email]}"
+  puts "ACCT #: #{account[:account_number]}"
+  puts "--------------------------------"
 end
 
 puts "Enter your account number"
 account_input = gets.chomp
 
-accounts.each do |hash|
-  if hash.has_value?(account_input)
-    puts "FIRST NAME: #{hash[:first_name]}"
-    puts "LAST NAME: #{hash[:last_name]}"
-    puts "EMAIL: #{hash[:email]}"
-    puts "ACCT #: #{hash[:account_number]}"
+puts "--------------------------------"
+
+puts "ACCOUNT: #{account_input} INFORMATION:"
+puts "--------------------------------"
+
+accounts.each do |account|
+  if account.has_value?(account_input)
+    puts "FIRST NAME: #{account[:first_name]}"
+    puts "LAST NAME: #{account[:last_name]}"
+    puts "EMAIL: #{account[:email]}"
+    puts "ACCT #: #{account[:account_number]}"
   end
 end
