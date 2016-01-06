953ac51be347bd911435d2821495a04d56163862
dianacamacho <dcam1901@yahoo.com>
2016-01-05 20:14:59 -0600

    samsally conditional fixes

--- bank.rb
+++ bank.rb
@@ -42,7 +42,6 @@ end
 puts "Enter your account number"
 account_input = gets.chomp
 
-
 accounts.each do |hash|
   if hash.has_value?(account_input)
     puts "FIRST NAME: #{hash[:first_name]}"
--- samsally.rb
+++ samsally.rb
@@ -6,8 +6,10 @@ sally_languages = gets.chomp.to_i
 
 if sam_recipes > 10 && sally_languages > 5
   puts "Sam should date Sally"
-else
+elsif sam_recipes <= 10 || sally_languages <= 5
   puts "Sam needs to find another girl"
+else
+  puts "please respond with an integer"
 end
 
 puts "Can Sam make crepes?"
@@ -18,7 +20,9 @@ sally_language = gets.chomp.downcase
   
 if sam_crepes == "yes" || sally_language == "yes"
   puts "Sam should marry Sally"
-else
+elsif sam_crepes == "no" || sally_language == "no"
   puts "Sam needs to find another girl"
+else
+  puts "please answer only yes or no"
 end
 
