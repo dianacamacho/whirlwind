83a117bfffb4c45a909145ac111cd32eebbc5651
dianacamacho <dcam1901@yahoo.com>
2016-01-05 21:22:10 -0600

    end of day updates

--- students.rb
+++ students.rb
@@ -4,14 +4,15 @@ while true
   p "enter name of student:"
   student_name = gets.chomp
   students << student_name
-   if student_name == "done"
+   if student_name == ""
     break
   end
 end
 
-students.delete("done")
+students.delete("")
 
 p students
+
 
 
 
